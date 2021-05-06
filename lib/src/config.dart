92e3dc320a498c20256ad1ff7132a03c3ee4fbbe
BradenBagby <braden.bagby@wavv.com>
2021-05-06 11:01:51 -0500

    config migrated

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,4 +1,4 @@
-// @dart=2.9
+
 
 import 'package:sip_ua/sip_ua.dart';
 
@@ -15,39 +15,39 @@ import 'utils.dart' as Utils;
 // Default settings.
 class Settings {
   // SIP authentication.
-  String authorization_user;
-  String password;
-  String realm;
-  String ha1;
+  String? authorization_user;
+  String? password;
+  String? realm;
+  String? ha1;
 
   // SIP account.
-  String display_name;
+  String? display_name;
   dynamic uri;
   dynamic contact_uri;
   String user_agent = DartSIP_C.USER_AGENT;
 
   // SIP instance id (GRUU).
-  String instance_id = null;
+  String? instance_id = null;
 
   // Preloaded SIP Route header field.
-  bool/*?*/ use_preloaded_route = false;
+  bool? use_preloaded_route = false;
 
   // Session parameters.
-  bool/*?*/ session_timers = true;
+  bool? session_timers = true;
   SipMethod session_timers_refresh_method = SipMethod.UPDATE;
-  int/*?*/ no_answer_timeout = 60;
+  int? no_answer_timeout = 60;
 
   // Registration parameters.
-  bool register = true;
-  int register_expires = 600;
+  bool? register = true;
+  int? register_expires = 600;
   dynamic registrar_server;
-  Map<String, dynamic> register_extra_contact_uri_params;
+  Map<String, dynamic>? register_extra_contact_uri_params;
 
   // Dtmf mode
-  DtmfMode/*?*/ dtmf_mode = DtmfMode.INFO;
+  DtmfMode? dtmf_mode = DtmfMode.INFO;
 
   // Connection options.
-  List<WebSocketInterface>/*!*/ sockets = <WebSocketInterface>[];
+  List<WebSocketInterface> sockets = <WebSocketInterface>[];
   int connection_recovery_max_interval = 30;
   int connection_recovery_min_interval = 2;
 
@@ -55,12 +55,12 @@ class Settings {
    * Host address.
    * Value to be set in Via sent_by and host part of Contact FQDN.
   */
-  String via_host = '${Utils.createRandomToken(12)}.invalid';
+  String? via_host = '${Utils.createRandomToken(12)}.invalid';
 
   // DartSIP ID
-  String jssip_id;
+  String? jssip_id;
 
-  String hostport_params;
+  String? hostport_params;
 }
 
 // Configuration checks.
@@ -106,10 +106,10 @@ class Checks {
       }
     }
   };
-  Map<String, Null Function(Settings src, Settings/*!*/ dst)> optional =
-      <String, Null Function(Settings src, Settings/*!*/ dst)>{
-    'authorization_user': (Settings src, Settings/*!*/ dst) {
-      String authorization_user = src.authorization_user;
+  Map<String, Null Function(Settings src, Settings dst)> optional =
+      <String, Null Function(Settings src, Settings dst)>{
+    'authorization_user': (Settings src, Settings dst) {
+      String? authorization_user = src.authorization_user;
       if (authorization_user == null) return;
       if (Grammar.parse('"$authorization_user"', 'quoted_string') == -1) {
         return;
@@ -117,30 +117,30 @@ class Checks {
         dst.authorization_user = authorization_user;
       }
     },
-    'user_agent': (Settings src, Settings/*!*/ dst) {
-      String/*?*/ user_agent = src.user_agent;
+    'user_agent': (Settings src, Settings dst) {
+      String? user_agent = src.user_agent;
       if (user_agent == null) return;
       if (user_agent is String) {
         dst.user_agent = user_agent;
       }
     },
-    'connection_recovery_max_interval': (Settings src, Settings/*!*/ dst) {
-      int/*?*/ connection_recovery_max_interval =
+    'connection_recovery_max_interval': (Settings src, Settings dst) {
+      int? connection_recovery_max_interval =
           src.connection_recovery_max_interval;
       if (connection_recovery_max_interval == null) return;
       if (connection_recovery_max_interval > 0) {
         dst.connection_recovery_max_interval = connection_recovery_max_interval;
       }
     },
-    'connection_recovery_min_interval': (Settings src, Settings/*!*/ dst) {
-      int/*?*/ connection_recovery_min_interval =
+    'connection_recovery_min_interval': (Settings src, Settings dst) {
+      int? connection_recovery_min_interval =
           src.connection_recovery_min_interval;
       if (connection_recovery_min_interval == null) return;
       if (connection_recovery_min_interval > 0) {
         dst.connection_recovery_min_interval = connection_recovery_min_interval;
       }
     },
-    'contact_uri': (Settings src, Settings/*!*/ dst) {
+    'contact_uri': (Settings src, Settings dst) {
       dynamic contact_uri = src.contact_uri;
       if (contact_uri == null) return;
       if (contact_uri is String) {
@@ -150,13 +150,13 @@ class Checks {
         }
       }
     },
-    'display_name': (Settings src, Settings/*!*/ dst) {
-      String display_name = src.display_name;
+    'display_name': (Settings src, Settings dst) {
+      String? display_name = src.display_name;
       if (display_name == null) return;
       dst.display_name = display_name;
     },
-    'instance_id': (Settings src, Settings/*!*/ dst) {
-      String instance_id = src.instance_id;
+    'instance_id': (Settings src, Settings dst) {
+      String? instance_id = src.instance_id;
       if (instance_id == null) return;
       if (instance_id.contains(RegExp(r'^uuid:', caseSensitive: false))) {
         instance_id = instance_id.substring(5);
@@ -167,56 +167,56 @@ class Checks {
         dst.instance_id = instance_id;
       }
     },
-    'no_answer_timeout': (Settings src, Settings/*!*/ dst) {
-      int/*?*/ no_answer_timeout = src.no_answer_timeout;
+    'no_answer_timeout': (Settings src, Settings dst) {
+      int? no_answer_timeout = src.no_answer_timeout;
       if (no_answer_timeout == null) return;
       if (no_answer_timeout > 0) {
         dst.no_answer_timeout = no_answer_timeout;
       }
     },
-    'session_timers': (Settings src, Settings/*!*/ dst) {
-      bool/*?*/ session_timers = src.session_timers;
+    'session_timers': (Settings src, Settings dst) {
+      bool? session_timers = src.session_timers;
       if (session_timers == null) return;
       if (session_timers is bool) {
         dst.session_timers = session_timers;
       }
     },
-    'session_timers_refresh_method': (Settings src, Settings/*!*/ dst) {
+    'session_timers_refresh_method': (Settings src, Settings dst) {
       SipMethod method = src.session_timers_refresh_method;
       if (method == SipMethod.INVITE || method == SipMethod.UPDATE) {
         dst.session_timers_refresh_method = method;
       }
     },
-    'password': (Settings src, Settings/*!*/ dst) {
-      String password = src.password;
+    'password': (Settings src, Settings dst) {
+      String? password = src.password;
       if (password == null) return;
       dst.password = password.toString();
     },
-    'realm': (Settings src, Settings/*!*/ dst) {
-      String realm = src.realm;
+    'realm': (Settings src, Settings dst) {
+      String? realm = src.realm;
       if (realm == null) return;
       dst.realm = realm.toString();
     },
-    'ha1': (Settings src, Settings/*!*/ dst) {
-      String ha1 = src.ha1;
+    'ha1': (Settings src, Settings dst) {
+      String? ha1 = src.ha1;
       if (ha1 == null) return;
       dst.ha1 = ha1.toString();
     },
-    'register': (Settings src, Settings/*!*/ dst) {
-      bool register = src.register;
+    'register': (Settings src, Settings dst) {
+      bool? register = src.register;
       if (register == null) return;
       if (register is bool) {
         dst.register = register;
       }
     },
-    'register_expires': (Settings src, Settings/*!*/ dst) {
-      int register_expires = src.register_expires;
+    'register_expires': (Settings src, Settings dst) {
+      int? register_expires = src.register_expires;
       if (register_expires == null) return;
       if (register_expires > 0) {
         dst.register_expires = register_expires;
       }
     },
-    'registrar_server': (Settings src, Settings/*!*/ dst) {
+    'registrar_server': (Settings src, Settings dst) {
       dynamic registrar_server = src.registrar_server;
       if (registrar_server == null) return;
       if (!registrar_server.contains(RegExp(r'^sip:', caseSensitive: false))) {
@@ -229,8 +229,8 @@ class Checks {
         dst.registrar_server = parsed;
       }
     },
-    'register_extra_contact_uri_params': (Settings src, Settings/*!*/ dst) {
-      Map<String, dynamic> register_extra_contact_uri_params =
+    'register_extra_contact_uri_params': (Settings src, Settings dst) {
+      Map<String, dynamic>? register_extra_contact_uri_params =
           src.register_extra_contact_uri_params;
       if (register_extra_contact_uri_params == null) return;
       if (register_extra_contact_uri_params is Map<String, dynamic>) {
@@ -238,15 +238,15 @@ class Checks {
             register_extra_contact_uri_params;
       }
     },
-    'use_preloaded_route': (Settings src, Settings/*!*/ dst) {
-      bool/*?*/ use_preloaded_route = src.use_preloaded_route;
+    'use_preloaded_route': (Settings src, Settings dst) {
+      bool? use_preloaded_route = src.use_preloaded_route;
       if (use_preloaded_route == null) return;
       if (use_preloaded_route is bool) {
         dst.use_preloaded_route = use_preloaded_route;
       }
     },
-    'dtmf_mode': (Settings src, Settings/*!*/ dst) {
-      DtmfMode/*?*/ dtmf_mode = src.dtmf_mode;
+    'dtmf_mode': (Settings src, Settings dst) {
+      DtmfMode? dtmf_mode = src.dtmf_mode;
       if (dtmf_mode == null) return;
       if (dtmf_mode is DtmfMode) {
         dst.dtmf_mode = dtmf_mode;
@@ -257,18 +257,18 @@ class Checks {
 
 final Checks checks = Checks();
 
-void load(Settings src, Settings/*!*/ dst) {
+void load(Settings src, Settings dst) {
   try {
     // Check Mandatory parameters.
     checks.mandatory
-        .forEach((String parameter, Null Function(Settings, Settings/*!*/) fun) {
+        .forEach((String parameter, Null Function(Settings, Settings) fun) {
       logger.info('Check mandatory parameter => $parameter.');
       fun(src, dst);
     });
 
     // Check Optional parameters.
     checks.optional
-        .forEach((String parameter, Null Function(Settings, Settings/*!*/) fun) {
+        .forEach((String parameter, Null Function(Settings, Settings) fun) {
       logger.debug('Check optional parameter => $parameter.');
       fun(src, dst);
     });
