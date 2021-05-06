9b68f24690d4542420efbc4142b7116ec72481b5
BradenBagby <braden.bagby@wavv.com>
2021-05-06 11:36:19 -0500

    event manager prepared

--- lib/src/event_manager/message_events.dart
+++ lib/src/event_manager/message_events.dart
@@ -6,6 +6,6 @@ import 'events.dart';
 class EventNewMessage extends EventType {
   EventNewMessage({this.message, this.originator, this.request});
   dynamic request;
-  String originator;
-  Message message;
+  String/*!*/ originator;
+  Message/*!*/ message;
 }
